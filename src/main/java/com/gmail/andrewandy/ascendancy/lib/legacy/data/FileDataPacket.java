fe74fa8665b26af8d6f42db88372c1a6e55c92ed
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2021-04-13 23:23:15 +0200

    Initial work on new io

---
+++ src/main/java/com/gmail/andrewandy/ascendancy/lib/legacy/data/FileDataPacket.java
@@ -0,0 +1,168 @@
+package com.gmail.andrewandy.ascendancy.lib.data;
+
+import com.gmail.andrewandy.ascendancy.lib.AscendancyPacket;
+import com.gmail.andrewandy.ascendancy.lib.results.AscendancyResultPacket;
+import com.gmail.andrewandy.ascendancy.lib.results.Result;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * Represents a packet which holds data from a given file on the disk.
+ */
+public class FileDataPacket extends DataPacket {
+
+    private static final AscendancyResultPacket BAD_DATA_PACKET = new AscendancyResultPacket("BAD_DATA_REC", Result.FAILURE);
+    private static final String PROTOCOL_VERSION = "1";
+    private static final String SPLITTER = "::";
+    private static String dataFolder;
+    private String fileName;
+    private long targetFileSize;
+
+    public FileDataPacket() {
+    }
+
+    public FileDataPacket(final UUID player) {
+        super(player);
+    }
+
+    public FileDataPacket(final UUID player, final File file) throws IOException {
+        this(player, new FileInputStream(file), file.getName(), file.length());
+    }
+
+
+    public FileDataPacket(final UUID player, final InputStream src, final String fileName, final long targetFileSize) throws
+            IOException {
+        super(player, src);
+        this.fileName = fileName;
+        if (targetFileSize < 0) {
+            throw new IllegalArgumentException("Invalid file size!");
+        }
+        this.targetFileSize = targetFileSize;
+    }
+
+    public static void setDataFolder(final File dataFolder) {
+        FileDataPacket.dataFolder = dataFolder.getAbsolutePath();
+    }
+
+    public static AscendancyPacket handleIncomingPacket(final FileDataPacket incoming) throws IOException {
+        final byte[] bytes = incoming.getData();
+        final String fileName = incoming.fileName;
+        if (dataFolder == null) {
+            throw new IllegalStateException("No data folder set!");
+        }
+        final File file = new File(dataFolder, fileName);
+        file.createNewFile();
+        try (final OutputStream os = new FileOutputStream(file)) {
+            os.write(bytes);
+        }
+        if (!incoming.fileSizeIsCorrect(file)) {
+            file.delete();
+            return BAD_DATA_PACKET;
+        }
+        return AscendancyResultPacket.SUCCESS;
+    }
+
+    @Override
+    public int fromBytes(final byte[] bytes) {
+        ByteBuf buf = ByteBufAllocator.DEFAULT.buffer(bytes.length);
+        buf.writeBytes(bytes);
+        final int idLength = Objects.requireNonNull(buf).readInt();
+        final String identifier = new String(buf.readSlice(idLength).array());
+        buf = buf.readBytes(idLength); //Since buf is immutable we must reassign.
+        final String[] arr = identifier.split(SPLITTER);
+        if (arr.length < 2) {
+            throw new IllegalArgumentException("Invalid identifier!");
+        }
+        try {
+            final Class<?> clazz = Class.forName(arr[0]);
+            if (!FileDataPacket.class.isAssignableFrom(clazz)) {
+                throw new IllegalArgumentException("Packet identifier not type of FileDataPacket!");
+            }
+            final String otherVersion = arr[1];
+            if (!otherVersion.equals(PROTOCOL_VERSION)) {
+                //Conversion
+            }
+        } catch (final ClassNotFoundException ex) {
+            throw new IllegalArgumentException("Packet identifier not type of FileDataPacket!");
+        }
+        final int nameLength = buf.readInt();
+        assert nameLength > 0;
+        final byte[] nameBytes = buf.readBytes(nameLength).slice().array();
+        fileName = new String(nameBytes);
+        targetFileSize = buf.readLong();
+        final int dataLen = buf.readInt();
+        final byte[] data = dataLen > 0 ? buf.readBytes(dataLen).slice().array() : new byte[0];
+        super.setData(data);
+        if (!fileSizeIsCorrect()) {
+            throw new IllegalStateException("File size != target file size!");
+        }
+        return buf.readerIndex();
+    }
+
+    private boolean fileSizeIsCorrect(final File file) {
+        return Objects.requireNonNull(file).length() == targetFileSize;
+    }
+
+    private boolean fileSizeIsCorrect() {
+        File temp = null;
+        try {
+            temp = File.createTempFile(UUID.randomUUID().toString(), ".tmp");
+            try (final FileOutputStream os = new FileOutputStream(temp)) {
+                os.write(getData());
+                return fileSizeIsCorrect(temp);
+            }
+        } catch (final IOException ex) {
+            return false;
+        } finally {
+            if (temp != null) {
+                temp.delete();
+            }
+        }
+    }
+
+
+    @Override
+    public byte[] getFormattedData() {
+        if (fileName == null) {
+            return new byte[0];
+        }
+        final ByteBuf buf = ByteBufAllocator.DEFAULT.buffer();
+        final byte[] nameBytes = fileName.getBytes();
+        final String identifier = getIdentifier();
+        final byte[] identifierBytes = identifier.getBytes();
+        final byte[] data = getData();
+        buf.writeInt(identifierBytes.length)
+                .writeBytes(identifierBytes)
+                .writeInt(nameBytes.length)
+                .writeBytes(nameBytes)
+                .writeLong(targetFileSize)
+                .writeInt(data.length)
+                .writeBytes(data);
+        return buf.array();
+    }
+
+    public void writeToFile(final File file) throws IOException {
+        if (file == null) {
+            throw new FileNotFoundException("null file!");
+        }
+        try (final OutputStream stream = new FileOutputStream(file)) {
+            writeToStream(stream, false);
+        }
+    }
+
+    @Override
+    public String getIdentifier() {
+        return FileRequestPacket.class.getCanonicalName() + SPLITTER + PROTOCOL_VERSION;
+    }
+
+}
---
+++ src/main/java/com/gmail/andrewandy/ascendancy/lib/legacy/data/FileRequestPacket.java
@@ -0,0 +1,91 @@
+package com.gmail.andrewandy.ascendancy.lib.data;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * Represents a packet to request for a file.
+ */
+public class FileRequestPacket extends DataRequestPacket {
+
+    public static final String PROTOCOL_VERSION = "1";
+    private static final String SPLITTER = "::";
+    private Path filePath;
+
+    public FileRequestPacket() {
+
+    }
+
+    public FileRequestPacket(final UUID player) {
+        super(player);
+    }
+
+    public FileRequestPacket(final FileRequestPacket other) {
+        if (other == null) {
+            return;
+        }
+        if (other.filePath != null) {
+            this.filePath = Paths.get(other.filePath.toString());
+        }
+        super.setTargetPlayer(other.getTargetPlayer());
+    }
+
+    public FileRequestPacket(final UUID player, final String filePath) {
+        this(player, Paths.get(filePath));
+    }
+
+    public FileRequestPacket(final UUID player, final Path path) {
+        this(player);
+        this.filePath = Objects.requireNonNull(path);
+    }
+
+    public Path getFilePath() {
+        return filePath;
+    }
+
+    @Override
+    public int fromBytes(final byte[] bytes) {
+        final ByteBuf buf = ByteBufAllocator.DEFAULT.buffer(bytes.length);
+        buf.writeBytes(bytes);
+        final byte[] rawString = buf.array();
+        final String str = new String(rawString);
+        final String[] split = str.split(SPLITTER);
+        if (split.length < 3) {
+            throw new IllegalArgumentException("Invalid buffer parsed!");
+        }
+        int index = 0;
+        final String classAsString = split[index++];
+        final String protocolVersion = split[index++];
+        final String filePath = split[index];
+        try {
+            final Class<?> clazz = Class.forName(classAsString);
+            if (!FileRequestPacket.class.isAssignableFrom(clazz)) {
+                throw new IllegalArgumentException("Invalid packet, not type of FileRequestPacket!");
+            }
+            if (!protocolVersion.equals(PROTOCOL_VERSION)) {
+                //Convert.
+            }
+            this.filePath = Paths.get(filePath);
+        } catch (final ClassNotFoundException ex) {
+            throw new IllegalArgumentException("Invalid packet, not type of FileRequestPacket!", ex);
+        }
+        return buf.readerIndex();
+    }
+
+    @Override
+    public byte[] getFormattedData() {
+        final String str = getClass().getCanonicalName() + SPLITTER + PROTOCOL_VERSION + SPLITTER + filePath.toString();
+        return str.getBytes();
+    }
+
+    @Override
+    public String getIdentifier() {
+        return FileRequestPacket.class.getCanonicalName() + SPLITTER + PROTOCOL_VERSION;
+    }
+
+}
